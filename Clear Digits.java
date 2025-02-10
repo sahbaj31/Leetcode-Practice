491215c7bd8d0e4e0a77fdcec1885b04c38e7171
Sahbaj Khan <sahbajkhan3236@gmail.com>
2025-02-10 20:29:04 +0530

    Create Clear Digits.java

---
+++ Clear Digits.java
@@ -0,0 +1,20 @@
+//3174. Clear Digits
+//https://leetcode.com/problems/clear-digits/description/?envType=daily-question&envId=2025-02-10
+
+class Solution {
+  public String clearDigits(String s) {
+    StringBuilder sb = new StringBuilder();
+
+    for (final char c : s.toCharArray())
+      if (Character.isDigit(c))
+        // Since `sb` only contains non-digit characters, popping the last
+        // character is equivalent to deleting the closest non-digit character.
+        sb.setLength(sb.length() - 1);
+      else
+        sb.append(c);
+
+    return sb.toString();
+  }
+}
+
+//O(n)
