02e705c22984bf380c8281dd6b17d26d235df608
Sahbaj Khan <sahbajkhan3236@gmail.com>
2025-03-18 00:05:31 +0530

    Time: 2 ms (98.83%), Space: 45.1 MB (29.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,32 @@
 class Solution {
     public int[] twoSum(int[] arr, int target) {
-        int n = arr.length;
-        int[] ans = new int[2];
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-                if((arr[i]+arr[j])== target){
-                    ans[0]=i;
-                    ans[1]=j;
-                }
+
+        //Optimal Soluting using: HashMap, T.C = O(n) ,  S.C = O(n)
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<arr.length; i++){
+            int complement = target - arr[i];
+            if(map.containsKey(complement)){
+                return new int[] {map.get(complement),i};
             }
+            map.put(arr[i],i);
         }
-        return ans;
+        return new int[] {};
+
+
+
+        //Brute Solution using Nested Loop; Time complexity= O(n^2)
+
+        // int n = arr.length;
+        // int[] ans = new int[2];
+        // for(int i=0; i<n-1; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         if((arr[i]+arr[j])== target){
+        //             ans[0]=i;
+        //             ans[1]=j;
+        //         }
+        //     }
+        // }
+        // return ans;
     }
 }
