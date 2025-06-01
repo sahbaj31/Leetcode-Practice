c03860f09bcf0e9d5a43b9e710679fd7bff67aa5
Sahbaj Khan <sahbajkhan3236@gmail.com>
2025-06-01 22:43:19 +0530

    2929. Distribute Candies Among Children II.java

---
+++ 2929. Distribute Candies Among Children II.java
@@ -0,0 +1,16 @@
+// 2929. Distribute Candies Among Children II
+// https://leetcode.com/problems/distribute-candies-among-children-ii/description/?envType=daily-question&envId=2025-06-01
+
+class Solution {
+    public long distributeCandies(int n, int limit) {
+        return combCount(n)
+             - 3 * combCount(n - (limit + 1))
+             + 3 * combCount(n - 2 * (limit + 1))
+             - combCount(n - 3 * (limit + 1));
+    }
+
+    private long combCount(long sum) {
+        if (sum < 0) return 0;
+        return (sum + 2) * (sum + 1) / 2;
+    }
+}
